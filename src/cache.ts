cf9ad0bc148821c4563a33eb1f940236e61fba7e
Christoffer Artmann <artgaard@gmail.com>
2025-02-03 15:11:27 +0100

    feat: Add a stats() method.

--- src/cache.ts
+++ src/cache.ts
@@ -1,11 +1,23 @@
 type FactoryFunction<K, V> = (key: K) => V
 
 interface CacheOptions {
+  collectStats: boolean
   maxSize: number
 }
 
 const defaultCacheOptions: CacheOptions = {
+  collectStats: true,
   maxSize: 100
+}
+
+interface CacheStats {
+  averageLoadPenalty: () => number
+  evictionCount: () => number
+  hitCount: () => number
+  hitRate: () => number
+  missCount: () => number
+  missRate: () => number
+  requestCount: () => number
 }
 
 export class Cache<K, V> {
@@ -14,6 +26,12 @@ export class Cache<K, V> {
 
   private readonly items: Map<K, V> = new Map()
   private keyAccessOrder: K[] = []
+
+  private accessCount = 0
+  private hitCount = 0
+  private missCount = 0
+  private evictionCount = 0
+  private loadPenaltyValues: number[] = []
 
   constructor(
     factoryFunction: FactoryFunction<K, V>,
@@ -29,6 +47,8 @@ export class Cache<K, V> {
   clear(): void {
     this.items.clear()
     this.keyAccessOrder = []
+
+    this.resetStats()
   }
 
   delete(key: K): boolean {
@@ -47,12 +67,26 @@ export class Cache<K, V> {
     if (cachedValue !== undefined) {
       this.updateKeyAccessOrder(key)
 
+      this.updateAccessStats('hit')
+
       return cachedValue
     }
 
+    const factoryStart = process.hrtime()
     const freshValue = this.factoryFunction(key)
+    const factoryEnd = process.hrtime(factoryStart)
+
+    const factoryDurationInMs = factoryEnd[0] * 1e3 + factoryEnd[1] / 1e6
+
+    this.loadPenaltyValues.push(factoryDurationInMs)
+
+    if (this.loadPenaltyValues.length >= 1000) {
+      this.loadPenaltyValues.shift()
+    }
 
     this.addItem(key, freshValue)
+
+    this.updateAccessStats('miss')
 
     return freshValue
   }
@@ -67,6 +101,18 @@ export class Cache<K, V> {
 
   size(): number {
     return this.items.size
+  }
+
+  stats(): CacheStats {
+    return {
+      averageLoadPenalty: () => this.loadPenaltyValues.length > 0 ? this.loadPenaltyValues.reduce((a, b) => a + b) / this.loadPenaltyValues.length : 0,
+      evictionCount: () => this.evictionCount,
+      hitCount: () => this.hitCount,
+      hitRate: () => this.accessCount > 0 ? this.hitCount / this.accessCount : 0,
+      missCount: () => this.missCount,
+      missRate: () => this.accessCount > 0 ? this.missCount / this.accessCount : 0,
+      requestCount: () => this.accessCount
+    }
   }
 
   private addItem(key: K, value: V): void {
@@ -87,6 +133,30 @@ export class Cache<K, V> {
     if (leastRecentlyUsedKey !== undefined) {
       this.items.delete(leastRecentlyUsedKey)
     }
+
+    this.updateEvictionCount()
+  }
+
+  private updateAccessStats(type: 'hit' | 'miss'): void {
+    this.accessCount++
+
+    if (type === 'hit') {
+      this.hitCount++
+    } else {
+      this.missCount++
+    }
+
+    if (this.accessCount >= Number.MAX_SAFE_INTEGER) {
+      this.resetStats()
+    }
+  }
+
+  private updateEvictionCount(): void {
+    this.evictionCount++
+
+    if (this.evictionCount >= Number.MAX_SAFE_INTEGER) {
+      this.resetStats()
+    }
   }
 
   private updateKeyAccessOrder(key: K): void {
@@ -97,5 +167,12 @@ export class Cache<K, V> {
     }
 
     this.keyAccessOrder.unshift(key)
+  }
+
+  private resetStats(): void {
+    this.accessCount = 0
+    this.evictionCount = 0
+    this.hitCount = 0
+    this.missCount = 0
   }
 }
