3e0e0f9aff8d4d05164a4bd392248689adf5c781
Christoffer Artmann <artgaard@gmail.com>
2025-02-03 15:11:27 +0100

    feat: Add a stats() method.

--- src/cache.test.ts
+++ src/cache.test.ts
@@ -11,11 +11,11 @@ describe('Cache', () => {
       cache.get(2)
       cache.get(3)
 
-      expect(cache.size()).toBe(3)
+      expect(cache.size()).toEqual(3)
 
       cache.clear()
 
-      expect(cache.size()).toBe(0)
+      expect(cache.size()).toEqual(0)
     })
   })
 
@@ -27,11 +27,11 @@ describe('Cache', () => {
       cache.get(2)
       cache.get(3)
 
-      expect(cache.size()).toBe(3)
+      expect(cache.size()).toEqual(3)
 
       cache.delete(2)
 
-      expect(cache.size()).toBe(2)
+      expect(cache.size()).toEqual(2)
 
       expect(cache.listTheCachedItemKeys()).toEqual([1, 3])
     })
@@ -41,13 +41,13 @@ describe('Cache', () => {
 
       cache.get(1)
 
-      expect(cache.delete(1)).toBe(true)
+      expect(cache.delete(1)).toEqual(true)
     })
 
     it('returns false if the key was not deleted.', () => {
       const cache = new Cache<number, number>((key) => key)
 
-      expect(cache.delete(1)).toBe(false)
+      expect(cache.delete(1)).toEqual(false)
     })
   })
 
@@ -55,13 +55,13 @@ describe('Cache', () => {
     it('returns values from the cache.', () => {
       const cache = new Cache<number, number>((key) => key)
 
-      expect(cache.get(1)).toBe(1)
-      expect(cache.get(1)).toBe(1)
-      expect(cache.get(1)).toBe(1)
+      expect(cache.get(1)).toEqual(1)
+      expect(cache.get(1)).toEqual(1)
+      expect(cache.get(1)).toEqual(1)
 
-      expect(cache.get(42)).toBe(42)
-      expect(cache.get(42)).toBe(42)
-      expect(cache.get(42)).toBe(42)
+      expect(cache.get(42)).toEqual(42)
+      expect(cache.get(42)).toEqual(42)
+      expect(cache.get(42)).toEqual(42)
     })
 
     it('uses the cached value if it exists.', () => {
@@ -69,9 +69,9 @@ describe('Cache', () => {
 
       const cache = new Cache(fetchNumber)
 
-      expect(cache.get(1)).toBe(1)
-      expect(cache.get(1)).toBe(1)
-      expect(cache.get(1)).toBe(1)
+      expect(cache.get(1)).toEqual(1)
+      expect(cache.get(1)).toEqual(1)
+      expect(cache.get(1)).toEqual(1)
 
       expect(fetchNumber).toHaveBeenCalledTimes(1)
     })
@@ -120,7 +120,7 @@ describe('Cache', () => {
 
       expect(() => cache.get(1)).toThrowError('Factory function error.')
 
-      expect(cache.size()).toBe(0)
+      expect(cache.size()).toEqual(0)
     })
   })
 
@@ -130,13 +130,104 @@ describe('Cache', () => {
 
       cache.get(1)
 
-      expect(cache.has(1)).toBe(true)
+      expect(cache.has(1)).toEqual(true)
     })
 
     it('returns false if the key does not exist in the cache.', () => {
       const cache = new Cache<number, number>((key) => key)
 
-      expect(cache.has(1)).toBe(false)
+      expect(cache.has(1)).toEqual(false)
+    })
+  })
+
+  describe('stats', () => {
+    it('returns the correct average load penalty.', () => {
+      const cache = new Cache<number, number>((key) => {
+        const start = process.hrtime()
+        while (process.hrtime(start)[1] < 1e6) { } // Simulate 1ms delay
+        return key
+      })
+      const stats = cache.stats()
+
+      expect(stats.averageLoadPenalty()).toEqual(0)
+
+      cache.get(1)
+      cache.get(2)
+      cache.get(3)
+
+      expect(stats.averageLoadPenalty()).toBeGreaterThan(0)
+    })
+
+    it('returns the correct eviction count.', () => {
+      const cache = new Cache<number, number>((key) => key, { maxSize: 2 })
+      const stats = cache.stats()
+
+      expect(stats.evictionCount()).toEqual(0)
+
+      cache.get(1)
+      cache.get(2)
+      cache.get(3)
+
+      expect(stats.evictionCount()).toEqual(1)
+    })
+
+    it('returns the correct hit count.', () => {
+      const cache = new Cache<number, number>((key) => key)
+      const stats = cache.stats()
+
+      cache.get(1)
+      cache.get(1)
+      cache.get(2)
+
+      expect(stats.hitCount()).toEqual(1)
+    })
+
+    it('returns the correct hit rate.', () => {
+      const cache = new Cache<number, number>((key) => key)
+      const stats = cache.stats()
+
+      expect(stats.hitRate()).toEqual(0)
+
+      cache.get(1)
+      cache.get(1)
+      cache.get(2)
+
+      expect(stats.hitRate()).toEqual(1 / 3)
+    })
+
+    it('returns the correct miss count.', () => {
+      const cache = new Cache<number, number>((key) => key)
+      const stats = cache.stats()
+
+      cache.get(1)
+      cache.get(1)
+      cache.get(2)
+
+      expect(stats.missCount()).toEqual(2)
+    })
+
+    it('returns the correct miss rate.', () => {
+      const cache = new Cache<number, number>((key) => key)
+      const stats = cache.stats()
+
+      expect(stats.missRate()).toEqual(0)
+
+      cache.get(1)
+      cache.get(1)
+      cache.get(2)
+
+      expect(stats.missRate()).toEqual(2 / 3)
+    })
+
+    it('returns the correct request count.', () => {
+      const cache = new Cache<number, number>((key) => key)
+      const stats = cache.stats()
+
+      cache.get(1)
+      cache.get(1)
+      cache.get(2)
+
+      expect(stats.requestCount()).toEqual(3)
     })
   })
 })
